2d9720174cef2e20640512a9492b917a7fa28c53
Samson <jjyung1234@gmail.com>
2023-09-25 11:40:44 +0800

    Add jsp

--- src/main/java/com/example/exploration/ExplorationApplication.java
+++ src/main/java/com/example/exploration/ExplorationApplication.java
@@ -2,11 +2,18 @@ package com.example.exploration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ExplorationApplication {
+public class ExplorationApplication extends SpringBootServletInitializer {
 
   public static void main(String[] args) {
     SpringApplication.run(ExplorationApplication.class, args);
+  }
+
+  @Override
+  protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+    return builder.sources(ExplorationApplication.class);
   }
 }
